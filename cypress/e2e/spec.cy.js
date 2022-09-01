1e635e3c41f6a2757f5001ea6cf592a15069a99b
haipinggong <ghaiping@hotmail.com>
2022-09-01 16:45:42 +1000

    Show the alias bug

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,21 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("page", () => {
+  it("works", () => {
+    cy.visit("https://example.cypress.io");
+    cy.get("body").children(".container").contains("a", "Querying").as("end");
+    cy.visit("https://example.cypress.io");
+
+    cy.get("@end");
+  });
+
+  it("does not work", () => {
+    // The test case works in Cypress 10.4.0
+    // The test case does not work in Cypress 10.5.0+
+
+    cy.visit("https://example.cypress.io");
+    cy.get2("body").children(".container").contains("a", "Querying").as("end");
+    cy.visit("https://example.cypress.io");
+
+    cy.get("@end");
+  });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,7 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("get2", (selector, ...options) => {
+  return cy.get(selector, ...options);
+});
