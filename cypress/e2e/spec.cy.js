eccc7c77a54e19247a152164928ce09c3d837f8f
haipinggong <ghaiping@hotmail.com>
2022-09-01 17:03:51 +1000

    Make it clearer

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,10 +2,10 @@
 describe("page", () => {
   it("works", () => {
     cy.visit("https://example.cypress.io");
-    cy.get("body").children(".container").contains("a", "Querying").as("end");
+    cy.get("body").children(".container").as("container");
     cy.visit("https://example.cypress.io");
 
-    cy.get("@end");
+    cy.get("@container");
   });
 
   it("does not work", () => {
@@ -13,9 +13,9 @@ describe("page", () => {
     // The test case does not work in Cypress 10.5.0+
 
     cy.visit("https://example.cypress.io");
-    cy.get2("body").children(".container").contains("a", "Querying").as("end");
+    cy.get2("body").children(".container").as("container");
     cy.visit("https://example.cypress.io");
 
-    cy.get("@end");
+    cy.get("@container");
   });
 });
