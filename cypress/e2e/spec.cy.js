e16ff0e5e6fd46ecc09dfaa3db09a2c0542136ef
haipinggong <ghaiping@hotmail.com>
2022-11-24 12:50:29 +1100

    Update the test name

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,12 +1,12 @@
 /// <reference types="cypress" />
 describe("page", () => {
-  it("tag1", { tags: "@tag1" }, () => {
+  it("tag1 should not be run", { tags: "@tag1" }, () => {
     cy.visit("https://example.cypress.io");
   });
-  it.only("tag2", { tags: ["@tag2"] }, () => {
+  it.only("tag2 should be run", { tags: ["@tag2"] }, () => {
     cy.visit("https://example.cypress.io");
   });
-  it("tag2 2", { tags: ["@tag2"] }, () => {
+  it("tag2 should not be run", { tags: ["@tag2"] }, () => {
     cy.visit("https://example.cypress.io");
   });
 });
